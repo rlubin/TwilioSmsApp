19b9eeb753b9c998a8736f7da2078fff5456690e
Ryan Lubin <ryan.lubin1@gmail.com>
2023-09-26 21:51:00 -0400

    added ability to send twilio sms message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using Microsoft.Extensions.Configuration;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json", true, true);
+        var config = builder.Build();
+
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        string accountSid = config["TWILIO_ACCOUNT_SID"];
+        string authToken = config["TWILIO_AUTH_TOKEN"];
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var message = MessageResource.Create(
+            body: "Test",
+            from: new Twilio.Types.PhoneNumber(config["FromPhoneNumber"]),
+            to: new Twilio.Types.PhoneNumber(config["ToPhoneNumber"])
+        );
+
+        Console.WriteLine(message.Sid);
+    }
+}
